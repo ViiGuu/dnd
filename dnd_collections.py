9d34d31b7fabdb0410ff6c5284e830a02e080fe8
Viggo Gustafsson <viggogustafsson@gmail.com>
2025-02-04 15:04:25 +0100

    removed num monsters

---
+++ dnd_collections.py
@@ -0,0 +1,378 @@
+
+    #NPC FEATURES
+
+npc_appearance = ['Distinctive jewelry, earrings, necklace, circlet, bracelets',
+                  'Piercings',
+                  'Flamboyant or outlandish clothes',
+                  'Formal, clean clothes',
+                  'Ragged, dirty clothes',
+                  'Pronounced scar',
+                  'Missing teeth',
+                  'Missing fingers',
+                  'Unusual eye colors or heterochromia',
+                  'Tattoos',
+                  'Birthmark',
+                  'Unusual skin color',
+                  'Bald',
+                  'Braided beard/hair',
+                  'Unusual hair color',
+                  'Nervous eye twitch',
+                  'Distinctive nose',
+                  'Crooked or rigid posture',
+                  'Exceptionally beautiful',
+                  'Exceptionally ugly',
+                  'Has a limp',
+                  'Visible burns.',
+                  'Distinctive walk',
+                  'Unusual height (extremely tall or short)',
+                  'Lisps',
+                  'Deformed or misshapen ears',
+                  'Unique dental modifications',
+                  'Pronounced muscle definition or unusual body build',
+                  'Noticeably fat or lean',
+                  'Distinctive hand tattoos or finger markings',
+                  ]
+
+npc_mannerism = ['Prone to singing, whistling, or humming quietly',
+                 'Speaks in rhyme or some other peculiar way',
+                 'Particularly low or high voice',
+                 'Slurs words, lisps or stutters',
+                 'Enunciates overly clearly',
+                 'Speaks loudly',
+                 'Whispers',
+                 'Uses flowery speech or long words',
+                 'Frequently uses the wrong word',
+                 'Uses colorful oaths and exclamations',
+                 'Makes constant jokes or puns',
+                 'Prone to predictions of doom',
+                 'Fidgets',
+                 'Squints',
+                 'Stares into the distance',
+                 'Chews something',
+                 'Paces',
+                 'Taps fingers',
+                 'Bites fingernails',
+                 'Twirls hair or tugs beard',
+                 'Has a nervous tic',
+                 'Adjusts clothing compulsively']
+
+npc_traits = ['Argumentative',
+              'Arrogant',
+              'Blustering',
+              'Rude',
+              'Curious',
+              'Friendly',
+              'Honest',
+              'Hot tempered',
+              'Irritable',
+              'Ponderous',
+              'Quiet',
+              'Suspicious',
+              'Paranoid']
+
+npc_races = ['dwarf',
+             'human',
+             'elf',
+             'halfling',
+             'gnome']
+
+    #NAMES
+
+dwarf_names_male = [['Tho', 'Thu', 'Gi', 'Du', 'Bro', 'Ki', 'Vi', 'Vin', 'Do', 'Da', 'Mo', 'Gra', 'Mo', 'Arn', 'O', 'Ga', 'Vo'],
+                    ['grym', 'grim', 'brim', 'ror', 'tur', 'li', 'lur', 'im', 'in', 'gun', 'gar', 'dis', 'rak', 'rums', 'ums', 
+                    'rik', 'gern', 'sik', 'dain', 'dal'],
+                    ['ga', 'ka', 'rath', 'ra', 'kha']]
+
+dwarf_names_female = [['Vis', 'Var', 'Hel', 'Ran', 'Maer', 'Am', 'Aes', 'Ael', 'Ar', 'Aud', 'Kath', 'Gur', 'Moir', 'Bir'],
+                      ['la', 'lys', 'lynn', 'ynn', 'ra', 'ta', 'dia', 'lia', 'waen', 'via', 'byr', 'tyn', 'ra', 'dis', 'na', 'nis', 'lita', 'ita'],
+                      ['al', 'en', 'in', 'ae']]
+
+human_names_male = [['Ald', 'Jor', 'Ler', 'Sig', 'Ever', 'Had', 'Ar', 'Mar', 'Al', 'Ker', 'And', 'Uth', 'Ath', 'Eth',
+                    'Bren', 'Cal', 'Dav', 'El', 'Fin', 'Gar', 'Hal', 'Ian', 'Kai', 'Lor', 'Ra'], #AI 
+                    ['ric', 'rim', 'nar', 'ner', 'reif', 'rian', 'mund', 'ret', 'ron', 'nam',
+                    'ken', 'den', 'van', 'in', 'lon', 'gan', 'ran', 'ian'], #AI
+                    ['dra', 'ka', 'ath', 'nir', 'ur', 'na', 'a', 'e', 'i', 'o',
+                    'la', 'ge', 'er', 'an', 'en', 'in', 'ien', 'on']] #AI
+
+human_names_female = [['Kat', 'Kit', 'El', 'Jan', 'Ja', 'Lea', 'Ya', 'Yan', 'Sa', 'Ai', 'Ei', 'Sar', 'San', 'Io', 'Al', 'Eld', 'Cael', 'Ae'],
+                    ['nie', 'na', 'lyn', 'wyn', 'wen', 'lia', 'die', 'ra'],
+                    ['no', 'ro', 'do', 'a']]
+
+elf_names_male = [
+    ['Ad', 'Ael', 'Ald', 'Ar', 'Ara', 'Aus', 'Ber', 'Bei', 'Car', 'En', 'Erd', 'Ere', 'Gal', 'Had', 'Hei', 'Hi',
+     'Imm', 'Ive', 'Lau', 'Min', 'Pae', 'Per', 'Qua', 'Ria', 'Rol', 'Sov', 'Thal', 'Ther', 'Var'],
+
+    ['ran', 'ar', 'il', 'is', 'o', 'an', 'en', 'ion'],
+
+    ['ram', 'am', 'an', 'el', 'ian', 'ric', 'lis', 'dan', 'ai', 'en', 'ion', 'len', 'liss', 'ior', 'vol', 'ren', 'dar', 'rel']
+]
+
+elf_names_female = [
+    ['A', 'E', 'Ad', 'Al', 'Anas', 'And', 'Ant', 'Beth', 'Bir', 'Cae', 'Dru', 'En', 'Fel', 'Iel', 'Jel', 'Key', 'Lesh', 
+     'Li', 'Mer', 'Mi', 'Na', 'Quel', 'Quil', 'Sar', 'Shan', 'Shaev', 'Sil', 'Thei', 'Thi', 'Vad', 'Val', 'Xan'],
+    ['rie', 'thaea', 'ianna', 'inua', 'na', 'nia', 'athe', 'ath', 'asta', 'ia', 'the', 'phia', 'ra', 'la', 'in', 'anna'],
+    ['stas', 'laste', 'naste', 'ryn', 'rel', 'lyn', 'lia', 'na', 'lial', 'ne', 'le', 'a', 'e', 'nan', 'lan', 'liel', 'riele', 'va',  've', 'len', 'lui']
+]
+
+halfling_names_male = [['Al', 'An', 'Ca', 'Cor', 'El', 'Er', 'Fin', 'Gar', 'Lin', 'Lyl', 'Mer', 'Mil', 'Os', 'Perr', 'Re', 'Rosc', 'Well'],
+                    ['ton', 'der', 'de', 'rin', 'don', 'rich', 'nan', 'dal', 'ric', 'born', 'rin', 'by']]
+halfling_names_female = [['Andr', 'Br', 'Call', 'Cor', 'Euphem', 'Jill', 'Kithr', 'Lavin', 'Lidd', 'Merl', 'Nedd', 'Pael', 'Port', 'Seraph',
+                    'Shaen', 'Tr', 'Van', 'Vern'],
+                    ['y', 'ee', 'ie', 'a', 'ia', 'ian', 'i', 'ina', 'ini']]
+
+gnome_names_male = [['Alst', 'Al', 'Bodd', 'Br', 'Burg', 'Dimb', 'Eld', 'Erk', 'Fonk', 'Bonk', 'Fr', 'Gerb', 'Gorb', 'Gimbl', 'Glim',
+                    'Jebedd', 'Kell', 'Namf', 'Orr', 'Roond', 'Seeb', 'Sindr', 'Warr', 'Wre', 'Zo'],
+                    ['on', 'yn', 'ock', 'occ', 'ell', 'le', 'in', 'oodle', 'o', 'e', 'im', 'i', 'enn']]
+gnome_names_female = [['Bimp', 'Breen', 'Caram', 'Carl', 'Don', 'Duvam', 'Elly', 'Lill', 'Loop', 'Boop', 'Bopp', 'Lorill', 'Mard', 'Niss',
+                    'Od', 'Orl', 'Royw', 'Sham', 'Tan', 'Wayw', "Zann", 'Zap'],
+                    ['nottin', 'a', 'y', 'ie', 'ip', 'in', 'ella', 'il', 'jobell', 'bell', 'wick', 'i', 'mottin', 'illa', 'nab', 'ix', 'wyn',
+                    'wocket']]
+
+    #TREASURE
+
+gemstones = {
+    10: [
+        "Azurite (opaque mottled deep blue)",
+        "Banded agate (translucent striped brown, blue, white, or red)",
+        "Blue quartz (transparent pale blue)",
+        "Eye agate (translucent circles of gray, white, brown, blue, or green)",
+        "Hematite (opaque gray-black)",
+        "Lapis lazuli (opaque light and dark blue with yellow flecks)",
+        "Malachite (opaque striated light and dark green)",
+        "Moss agate (translucent pink or yellow-white with mossy gray or green markings)",
+        "Obsidian (opaque black)",
+        "Rhodochrosite (opaque light pink)",
+        "Tiger eye (translucent brown with golden center)",
+        "Turquoise (opaque light blue-green)"
+    ],
+    50: [
+        "Bloodstone (opaque dark gray with red flecks)",
+        "Carnelian (opaque orange to red-brown)",
+        "Chalcedony (opaque white)",
+        "Chrysoprase (translucent green)",
+        "Citrine (transparent pale yellow-brown)",
+        "Jasper (opaque blue, black, or brown)",
+        "Moonstone (translucent white with pale blue glow)",
+        "Onyx (opaque bands of black and white, or pure black or white)",
+        "Quartz (transparent white, smoky gray, or yellow)",
+        "Sardonyx (opaque bands of red and white)",
+        "Star rose quartz (translucent rosy stone with white star-shaped center)",
+        "Zircon (transparent pale blue-green)"
+    ],
+    100: [
+        "Amber (transparent watery gold to rich gold)",
+        "Amethyst (transparent deep purple)",
+        "Chrysoberyl (transparent yellow-green to pale green)",
+        "Coral (opaque crimson)",
+        "Garnet (transparent red, brown-green, or violet)",
+        "Jade (translucent light green, deep green, or white)",
+        "Jet (opaque deep black)",
+        "Pearl (opaque lustrous white, yellow, or pink)",
+        "Spinel (transparent red, red-brown, or deep green)",
+        "Tourmaline (transparent pale green, blue, brown, or red)"
+    ],
+    500: [
+        "Alexandrite (transparent dark green)",
+        "Aquamarine (transparent pale blue-green)",
+        "Black pearl (opaque pure black)",
+        "Blue spinel (transparent deep blue)",
+        "Peridot (transparent rich olive green)",
+        "Topaz (transparent golden yellow)",
+        "Black sapphire (translucent lustrous black with glowing highlights)",
+        "Diamond (transparent blue-white, canary, pink, brown, or blue)",
+        "Jacinth (transparent fiery orange)",
+        "Ruby (transparent clear red to deep crimson)"
+    ],
+    1000: [
+        "Black opal (translucent dark green with black mottling and golden flecks)",
+        "Blue sapphire (transparent blue-white to medium blue)",
+        "Emerald (transparent deep bright green)",
+        "Fire opal (translucent fiery red)",
+        "Opal (translucent pale blue with green and golden mottling)",
+        "Star ruby (translucent ruby with white star-shaped center)",
+        "Star sapphire (translucent blue sapphire with white star-shaped center)",
+        "Yellow sapphire (transparent fiery yellow or yellow-green)"
+    ]
+}
+
+art_objects = {
+    25: [
+        "Silver ewer",
+        "Carved bone statuette",
+        "Small gold bracelet",
+        "Cloth-of-gold vestments",
+        "Black velvet mask stitched with silver thread",
+        "Copper chalice with silver filigree",
+        "Pair of engraved bone dice",
+        "Small mirror set in a painted wooden frame",
+        "Embroidered silk handkerchief",
+        "Gold locket with a painted portrait inside"
+    ],
+    250: [
+        "Gold ring set with bloodstones",
+        "Carved ivory statuette",
+        "Large gold bracelet",
+        "Silver necklace with a gemstone pendant",
+        "Bronze crown",
+        "Silk robe with gold embroidery",
+        "Large well-made tapestry",
+        "Brass mug with jade inlay",
+        "Box of turquoise animal figurines",
+        "Gold bird cage with electrum filigree"
+    ],
+    750: [
+        "Silver chalice set with moonstones",
+        "Silver-plated steel longsword with jet set in hilt",
+        "Carved harp of exotic wood with ivory inlay and zircon gems",
+        "Small gold idol",
+        "Gold dragon comb set with red garnets as eyes",
+        "Bottle stopper cork embossed with gold leaf and set with amethysts",
+        "Ceremonial electrum dagger with a black pearl in the pommel",
+        "Silver and gold brooch",
+        "Obsidian statuette with gold fittings and inlay",
+        "Painted gold war mask"
+    ],
+    2500: [
+        "Fine gold chain set with a fire opal",
+        "Old masterpiece painting",
+        "Embroidered silk and velvet mantle set with numerous moonstones",
+        "Platinum bracelet set with a sapphire",
+        "Embroidered glove set with jewel chips",
+        "Jeweled anklet",
+        "Gold music box",
+        "Gold circlet set with four aquamarines",
+        "Eye patch with a mock eye set in blue sapphire and moonstone",
+        "A necklace string of small pink pearls"
+    ],
+    7500: [
+        "Jeweled gold crown",
+        "Jeweled platinum ring",
+        "Small gold statuette set with rubies",
+        "Gold cup set with emeralds",
+        "Gold jewelry box with platinum filigree",
+        "Painted gold child's sarcophagus",
+        "Jade game board with solid gold playing pieces",
+        "Bejeweled ivory drinking horn with gold filigree"
+    ]
+}
+
+trinkets = [
+    "A mummified goblin hand",
+    "A piece of crystal that faintly glows in the moonlight",
+    "A gold coin minted in an unknown land",
+    "A diary written in a language you don't know",
+    "A brass ring that never tarnishes",
+    "An old chess piece made from glass",
+    "A pair of knucklebone dice, each with a skull symbol on the side that would normally show six pips",
+    "A small idol depicting a nightmarish creature that gives you unsettling dreams when you sleep near it",
+    "A rope necklace from which dangles four mummified elf fingers",
+    "The deed for a parcel of land in a realm unknown to you",
+    "A 1-ounce block made from an unknown material",
+    "A small cloth doll skewered with needles",
+    "A tooth from an unknown beast",
+    "A bright green feather",
+    "An enormous scale, perhaps from a dragon",
+    "An old divination card bearing your likeness",
+    "A glass orb filled with moving smoke",
+    "A 1-pound egg with a bright red shell",
+    "A pipe that blows bubbles",
+    "A glass jar containing a weird bit of flesh floating in pickling fluid",
+    "A tiny gnome-crafted music box that plays a song you dimly remember from your childhood",
+    "A small wooden statuette of a smug halfling",
+    "A brass orb etched with strange runes",
+    "A multicolored stone disk",
+    "A tiny silver icon of a raven",
+    "A bag containing forty-seven humanoid teeth, one of which is rotten",
+    "A shard of obsidian that always feels warm to the touch",
+    "A dragon's bony talon hanging from a plain leather necklace",
+    "A pair of old socks",
+    "A blank book whose pages refuse to hold ink, chalk, graphite, or any other substance or marking",
+    "A silver badge in the shape of a five-pointed star",
+    "A knife that belonged to a relative",
+    "A glass vial filled with nail clippings",
+    "A rectangular metal device with two tiny metal cups on one end that throws sparks when wet",
+    "A white, sequined glove sized for a human",
+    "A vest with one hundred tiny pockets",
+    "A small, weightless stone block",
+    "A tiny sketch portrait of a goblin",
+    "An empty glass vial that smells of perfume when opened",
+    "A gemstone that looks like a lump of coal when examined by anyone but you",
+    "A scrap of cloth from an old banner",
+    "A rank insignia from a lost legionnaire",
+    "A tiny silver bell without a clapper",
+    "A mechanical canary inside a gnomish lamp",
+    "A tiny chest carved to look like it has numerous feet on the bottom",
+    "A dead sprite inside a clear glass bottle",
+    "A metal can that has no opening but sounds as if it is filled with liquid, sand, spiders, or broken glass (your choice)",
+    "A glass orb filled with water, in which swims a clockwork goldfish",
+    "A silver spoon with an M engraved on the handle",
+    "A whistle made from gold-colored wood",
+    "A dead scarab beetle the size of your hand",
+    "Two toy soldiers, one with a missing head",
+    "A small box filled with different-sized buttons",
+    "A candle that can't be lit",
+    "A tiny cage with no door",
+    "An old key",
+    "An indecipherable treasure map",
+    "A hilt from a broken sword",
+    "A rabbit's foot",
+    "A glass eye",
+    "A cameo carved in the likeness of a hideous person",
+    "A silver skull the size of a coin",
+    "An alabaster mask",
+    "A pyramid of sticky black incense that smells very bad",
+    "A nightcap that, when worn, gives you pleasant dreams",
+    "A single caltrop made from bone",
+    "A gold monocle frame without the lens",
+    "A 1-inch cube, each side painted a different color",
+    "A crystal knob from a door",
+    "A small packet filled with pink dust",
+    "A fragment of a beautiful song, written as musical notes on two pieces of parchment",
+    "A silver teardrop earring made from a real teardrop",
+    "The shell of an egg painted with scenes of human misery in disturbing detail",
+    "A fan that, when unfolded, shows a sleeping cat",
+    "A set of bone pipes",
+    "A four-leaf clover pressed inside a book discussing manners and etiquette",
+    "A sheet of parchment upon which is drawn a complex mechanical contraption",
+    "An ornate scabbard that fits no blade you have found so far",
+    "An invitation to a party where a murder happened",
+    "A bronze pentacle with an etching of a rat's head in its center",
+    "A purple handkerchief embroidered with the name of a powerful archmage",
+    "Half of a floorplan for a temple, castle, or some other structure",
+    "A bit of folded cloth that, when unfolded, turns into a stylish cap",
+    "A receipt of deposit at a bank in a far-flung city",
+    "A diary with seven missing pages",
+    "An empty silver snuffbox bearing an inscription on the surface that says 'dreams'",
+    "An iron holy symbol devoted to an unknown god",
+    "A book that tells the story of a legendary hero's rise and fall, with the last chapter missing",
+    "A vial of dragon blood",
+    "An ancient arrow of elven design",
+    "A needle that never bends",
+    "An ornate brooch of dwarven design",
+    "An empty wine bottle bearing a pretty label that says 'The Wizard of Wines Winery, Red Dragon Crush, 331422-W'",
+    "A mosaic tile with a multicolored, glazed surface",
+    "A petrified mouse",
+    "A black pirate flag adorned with a dragon's skull and crossbones",
+    "A tiny mechanical crab or spider that moves about when it's not being observed",
+    "A glass jar containing lard with a label that reads 'Griffon Grease'",
+    "A wooden box with a ceramic bottom that holds a living worm with a head on each end of its body",
+    "A metal urn containing the ashes of a hero"
+]
+
+magic_items_a = {
+    "01-50": "Potion of healing",
+    "51-60": "Spell scroll (cantrip)",
+    "61-70": "Potion of climbing", 
+    "71-90": "Spell scroll (1st level)",
+    "91-94": "Spell scroll (2nd level)",
+    "95-98": "Potion of greater healing",
+    "99-99": "Bag of holding",
+    "100-100": "Driftglobe"
+}
+
+def magic_items(table):
+    match table:
+        case "a":
+            return magic_items_a
+        case _:
+            print("Magic items table not found")
